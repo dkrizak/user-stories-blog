8c612b7169188d959db198f9abffd0f8c61582ad
Denis Križak <denis.krizak@brightstraining.com>
2022-11-11 10:06:48 +0100

    Initial commit

---
+++ src/main/java/hr/project/userstoriesblog/UserStoriesBlogApplication.java
@@ -0,0 +1,13 @@
+package hr.project.userstoriesblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserStoriesBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserStoriesBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/project/userstoriesblog/UserStoriesBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.project.userstoriesblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserStoriesBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
